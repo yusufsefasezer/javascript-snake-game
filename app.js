e4971c9a743eb5f3978bd4ca2acdc38d4308a865
Yusuf Sezer <yusufsezer@mail.com>
2023-02-20 21:52:45 +0300

    more structured and performant.

--- app.js
+++ app.js
@@ -1,53 +1,30 @@
-"use strict";
-var gameStart = null,
-  gameSpeed = null,
-  gameArea = null,
-  gameAreaContext = null,
+'use strict';
+
+var gameStart = {},
+  gameSpeed = {},
+  gameArea = {},
+  gameAreaContext = {},
+  snake = [],
   gameAreaWidth = 0,
   gameAreaHeight = 0,
   cellWidth = 0,
   playerScore = 0,
-  snake = null,
-  snakeFood = null,
-  snakeDirection = null,
+  snakeFood = {},
+  snakeDirection = '',
   speedSize = 0,
-  timer = null;
+  timer = {};
 
-function initialize() {
-  gameStart = document.querySelector("#gameStart");
-  gameSpeed = document.querySelector("#gameSpeed");
-  gameArea = document.querySelector("#gameArea");
-  gameAreaContext = gameArea.getContext("2d");
+function initElement() {
+  gameStart = document.querySelector('#gameStart');
+  gameSpeed = document.querySelector('#gameSpeed');
+  gameArea = document.querySelector('#gameArea');
+
+  gameAreaContext = gameArea.getContext('2d');
   gameAreaWidth = 400;
   gameAreaHeight = 600;
   cellWidth = 20;
   gameArea.width = gameAreaWidth;
   gameArea.height = gameAreaHeight;
-
-  gameStart.onclick = function () {
-    this.disabled = true;
-    startGame();
-  }
-}
-
-function startGame() {
-  playerScore = 0;
-  snakeDirection = "right";
-  speedSize = parseInt(gameSpeed.value);
-
-  if (speedSize > 9) {
-    speedSize = 9;
-  } else if (speedSize < 0) {
-    speedSize = 1;
-  }
-
-  snake = [];
-  snake.push({ x: 0, y: cellWidth });
-
-  createFood();
-
-  clearInterval(timer);
-  timer = setInterval(createGameArea, 500 / speedSize);
 }
 
 function createFood() {
@@ -57,26 +34,45 @@ function createFood() {
   };
 }
 
+function control(x, y, array) {
+  for (var index = 0, length = array.length; index < length; index++) {
+    if (array[index].x == x && array[index].y == y) return true;
+  }
+  return false;
+}
+
+function writeScore() {
+  gameAreaContext.font = '50px sans-serif';
+  gameAreaContext.fillStyle = '#FF0000';
+  gameAreaContext.fillText('Score: ' + playerScore, (gameAreaWidth / 2) - 100, gameAreaHeight / 2);
+}
+
+function createSquare(x, y) {
+  gameAreaContext.fillStyle = '#000000';
+  gameAreaContext.fillRect(x * cellWidth, y * cellWidth, cellWidth, cellWidth);
+}
+
 function createGameArea() {
   var snakeX = snake[0].x;
   var snakeY = snake[0].y;
 
-  gameAreaContext.fillStyle = "#FFFFFF";
+  gameAreaContext.fillStyle = '#FFFFFF';
   gameAreaContext.fillRect(0, 0, gameAreaWidth, gameAreaHeight);
-  gameAreaContext.strokeStyle = "#000000";
+
+  gameAreaContext.strokeStyle = '#000000';
   gameAreaContext.strokeRect(0, 0, gameAreaWidth, gameAreaHeight);
 
-  if (snakeDirection == "right") {
+  if (snakeDirection == 'right') {
     snakeX++;
-  } else if (snakeDirection == "left") {
+  } else if (snakeDirection == 'left') {
     snakeX--;
-  } else if (snakeDirection == "down") {
+  } else if (snakeDirection == 'down') {
     snakeY++;
-  } else if (snakeDirection == "up") {
+  } else if (snakeDirection == 'up') {
     snakeY--;
   }
 
-  if ((snakeX == -1) || (snakeX == gameAreaWidth / cellWidth) || (snakeY == -1) || (snakeY == gameAreaHeight / cellWidth) || Control(snakeX, snakeY, snake)) {
+  if ((snakeX == -1) || (snakeX == gameAreaWidth / cellWidth) || (snakeY == -1) || (snakeY == gameAreaHeight / cellWidth) || control(snakeX, snakeY, snake)) {
     writeScore();
     clearInterval(timer);
     gameStart.disabled = false;
@@ -95,38 +91,55 @@ function createGameArea() {
 
   snake.unshift(newHead);
 
-  for (var i = 0; i < snake.length; i++) {
-    createSquare(snake[i].x, snake[i].y);
+  for (var index = 0, length = snake.length; index < length; index++) {
+    createSquare(snake[index].x, snake[index].y);
   }
 
   createSquare(snakeFood.x, snakeFood.y);
 }
 
-function Control(x, y, array) {
-  for (var i = 0; i < array.length; i++) {
-    if (array[i].x == x && array[i].y == y) return true;
+function startGame() {
+  snake = [];
+  snake.push({ x: 0, y: cellWidth });
+
+  createFood();
+
+  clearInterval(timer);
+  timer = setInterval(createGameArea, 500 / speedSize);
+}
+
+function onStartGame() {
+  this.disabled = true;
+
+  playerScore = 0;
+  snakeDirection = 'right';
+  speedSize = parseInt(gameSpeed.value);
+
+  if (speedSize > 9) {
+    speedSize = 9;
+  } else if (speedSize < 0) {
+    speedSize = 1;
   }
-  return false;
-}
 
-function writeScore() {
-  gameAreaContext.font = "50px sans-serif";
-  gameAreaContext.fillStyle = "#FF0000";
-  gameAreaContext.fillText("Score: " + playerScore, (gameAreaWidth / 2) - 100, gameAreaHeight / 2);
-}
-
-function createSquare(x, y) {
-  gameAreaContext.fillStyle = "#000000";
-  gameAreaContext.fillRect(x * cellWidth, y * cellWidth, cellWidth, cellWidth);
+  startGame();
 }
 
 function changeDirection(e) {
   var keys = e.which;
-  if (keys == "40" && snakeDirection != "up") snakeDirection = "down";
-  else if (keys == "39" && snakeDirection != "left") snakeDirection = "right";
-  else if (keys == "38" && snakeDirection != "down") snakeDirection = "up";
-  else if (keys == "37" && snakeDirection != "right") snakeDirection = "left";
+  if (keys == '40' && snakeDirection != 'up') snakeDirection = 'down';
+  else if (keys == '39' && snakeDirection != 'left') snakeDirection = 'right';
+  else if (keys == '38' && snakeDirection != 'down') snakeDirection = 'up';
+  else if (keys == '37' && snakeDirection != 'right') snakeDirection = 'left';
 }
 
-window.onkeydown = changeDirection;
-window.onload = initialize;
+function initEvent() {
+  gameStart.addEventListener('click', onStartGame);
+  window.addEventListener('keydown', changeDirection);
+}
+
+function init() {
+  initElement();
+  initEvent();
+}
+
+window.addEventListener('DOMContentLoaded', init);
